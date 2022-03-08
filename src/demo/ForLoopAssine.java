a8626793ff2546b28e1a7af6032b13f5542d7a06
manoj <manojabeta@gmail.com>
2022-03-08 20:31:36 +0530

    sys

--- src/demo/ForLoopAssine.java
+++ src/demo/ForLoopAssine.java
@@ -3,15 +3,14 @@ package demo;
 public class ForLoopAssine {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int k=1;
-		for (int i=0;i<4;i++) {
-//			System.out.println("outer loop started " +k);
-			for (int j=0;j<1+i;j++) {
-				System.out.print(+k);
-				k++;
+	int k=3;
+		for (int i=0;i<3;i++) {
+			for (int j=0;j<=i;j++) {
+				System.out.print(k + " ");
+				k=k+3;
 				
 			}
+			
 			System.out.println("");
 		}
 	}
