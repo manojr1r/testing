42a2c7a165912c77ddcb8529b09be1a006655269
Arjun Medicals <manojabeta@gmail.com>
2022-03-08 23:54:23 +0530

    MulDimminNo

--- src/javaPractice/MultiDimensionalArray.java
+++ src/javaPractice/MultiDimensionalArray.java
@@ -3,18 +3,22 @@ package javaPractice;
 public class MultiDimensionalArray {
 
 	public static void main(String[] args) {
-		int a[][] = {{4,5,6},{1,2,3}};
-		
+		int a[][] = {{7,5,6},{56,87,90}};
+		int min= a[0][0];
 		
 		for (int i = 0; i<2; i++)
 		{
 			for (int j = 0; j<3; j++)
 			{
-				System.out.print(a[i][j]);
+				if (min > (a[i][j]))
+				{
+					min =(a[i][j]);
+				}
+				
 			}
-			System.out.println("");
+			
 		}
-
+		System.out.println(min);
 	}
 
 }
